719e20e3af3b4e3f382265de157f22245c0f0b9c
miftahov <miftahov@alkor.co.ru>
2025-08-10 00:19:28 +0300

    add posts list

--- app/src/main/java/miftalm/intro/MainActivity.kt
+++ app/src/main/java/miftalm/intro/MainActivity.kt
@@ -3,14 +3,6 @@ package miftalm.intro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import miftalm.intro.ui.PostsScreen
 import miftalm.intro.ui.PostsViewModel
 import miftalm.intro.ui.theme.IntroTheme
--- app/src/main/java/miftalm/intro/ui/PostsScreen.kt
+++ app/src/main/java/miftalm/intro/ui/PostsScreen.kt
@@ -1,13 +1,21 @@
 package miftalm.intro.ui
 
-import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import miftalm.intro.domain.Post
 import miftalm.intro.ui.state.PostsState
 
 @Composable
@@ -19,9 +27,14 @@ fun PostsScreen(viewModel: PostsViewModel) {
             Text("loading")
         }
         is PostsState.Success -> {
-           LazyColumn {
+           LazyColumn(
+               modifier = Modifier
+                   .fillMaxSize()
+                   .statusBarsPadding()
+                   .navigationBarsPadding()
+           ) {
                 items(currentState.posts) {
-                    Text(it.id.toString())
+                    PostItem(it)
                 }
             }
         }
@@ -29,5 +42,22 @@ fun PostsScreen(viewModel: PostsViewModel) {
             Text(text = "Error: ${currentState.message}")
         }
 
+    }
+}
+
+@Composable
+private fun PostItem(post: Post) {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text(
+            text = post.title,
+            color = Color.Red,
+            fontSize = 20.sp
+        )
+        Text(
+            text = post.body,
+            fontSize = 16.sp
+        )
     }
 }
